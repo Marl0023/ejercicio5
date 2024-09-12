1ef3859cfecb3da42444bd777f2e79d560e2602c
Marl0023 <marlotay2312@gmail.com>
2024-09-12 08:26:39 -0500

    Primer Salvado

---
+++ ej1/Program.cs
@@ -0,0 +1,37 @@
+﻿int sumaP=0, sumaI=0;
+Console.WriteLine("Ingrese los numeros que desea escepto negativos, [0] finaliza el programa");
+int num = int.Parse(Console.ReadLine());
+int i = 1;
+if (num == 0) {
+    i = 0;
+}
+while (i > 0) {
+    Console.WriteLine("Ingrese el siguiente valor");
+    num = int.Parse(Console.ReadLine());
+    if (num == 0)
+    {
+        i = 0;
+    }
+    else {
+        if (num < 0)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Numero no registrado, vuelva a intentarlo");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        else {
+            if (num % 2 == 0)
+            {
+                sumaP += num;
+            }
+            else
+            {
+                sumaI += num;
+            }
+        }
+    }
+    
+}
+Console.WriteLine("Suma de los pares: "+sumaP);
+Console.WriteLine("Suma de los impares: "+sumaI);
+
---
+++ ej2/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Ingrese el valor de los lados: ");
+Console.Write("Alto: ");
+int alto  = int.Parse(Console.ReadLine());
+Console.Write("Ancho: ");
+int ancho= int.Parse(Console.ReadLine());
+
